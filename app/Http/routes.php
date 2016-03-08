7c9b4edf1ecdd4b30cd80a3d3eedb45ac903c158
egg egg <eggegg01@126.com>
2016-03-08 06:11:58 +0000

    Create a new book

---
+++ app/Http/routes.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$app->get('/', function () use ($app) {
+    return $app->welcome();
+});
+
+$app->get('/books', 'BooksController@index');
+$app->get('/books/{id:[\d]+}', [
+	'as' => 'books.show',
+	'uses' => 'BooksController@show'
+]);
+$app->post('/books', 'BooksController@store');
